1fad0d3ec1a211ebad77dc2316794a27f843422b
SamLynch16 <spl8631@rit.edu>
2025-03-05 19:58:43 -0500

    first commit

--- src/server.js
+++ src/server.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../client'), { index: 'client.html' }));
-app.use('/docs', express.static(path.join(__dirname, '../documentation')));
+aapp.use('/docs', express.static(path.join(__dirname, '../documentation'), { index: 'documentation.html' }));
+
 
 
 const pokedexData = JSON.parse(
